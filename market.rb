40b5ba3ba9f7d40da1b202fa8c95e4d168b0f336
CoVEX Coin <36303523+Covexcoin@users.noreply.github.com>
2020-09-12 14:32:56 -0700

    Add files via upload

---
+++ market.rb
@@ -0,0 +1,8 @@
+module Cryptoexchange::Exchanges
+  module Covex
+    class Market < Cryptoexchange::Models::Market
+      NAME = 'covex'
+      API_URL = 'https://covex.io/TestingCode/'
+    end
+  end
+end
